fc256a58e36956f7a41c1f33fc39f3385fd1d427
VladislavMaksimov <workmaximov@yandex.ru>
2021-04-21 18:47:00 +0500

    Add class for field generating.

--- DriveMotor/DriveMotor.py
+++ DriveMotor/DriveMotor.py
@@ -1,24 +1,4 @@
-def fill_row() -> list:
-    row = []
-    row.extend(['-'] + ['.' for j in range(18)] + ['-'])
-    return row
+from field import Field
 
-def initialize() -> list:
-    field = []
-    for i in range(20):
-        row = []
-        if i == 0 or i == 19:
-            row.extend(['-' for j in range(20)])
-        else:
-            row.extend(fill_row())
-        field.append(row)
-    return field
-
-def print_test(field):
-    for i in range(20):
-        row = ''
-        for j in range(20):
-            row += field[i][j]
-        print(row)
-
-print_test(initialize())
+field = Field(10, 20)
+field.print()
---
+++ DriveMotor/field.py
@@ -0,0 +1,31 @@
+class Field(object):
+    """Генерирует поле для робота."""
+
+    def __init__(self, height, width):
+        self.field = []
+        self.__height = height
+        self.__width = width
+        self.__initialize()
+
+    def __fill_row(self) -> list:
+        row = []
+        row.extend(['-'] + ['.' for j in range(18)] + ['-'])
+        return row
+
+    def __initialize(self) -> list:
+        field = []
+        for i in range(self.__height):
+            row = []
+            if i == 0 or i == self.__height - 1:
+                row.extend(['-' for j in range(self.__width)])
+            else:
+                row.extend(self.__fill_row())
+            field.append(row)
+        self.field = field
+
+    def print(self):
+        for i in range(self.__height):
+            row = ''
+            for j in range(self.__width):
+                row += self.field[i][j]
+            print(row)
